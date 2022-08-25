6dd303b9263cb9a964f5f485efa99872ab71042c
Dylan Pruitt <dpruitt0703@gmail.com>
2022-08-25 07:47:29 -0400

    Added path require

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const path = require("path");
 const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
