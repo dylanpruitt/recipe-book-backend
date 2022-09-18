93c2abfd95506c8d62359da5406350282836bead
Dylan Pruitt <dpruitt0703@gmail.com>
2022-09-18 12:08:36 -0400

    Modified server.js to actually be able to send UploadStatus data through Socket.IO

--- server.js
+++ server.js
@@ -52,10 +52,9 @@ async function handleSubmission(socket, item) {
     console.log(results);
     if (results != null) {
       addRecipeToListing(item, socket);
-      console.log(UploadStatus.SUCCESS);
-      io.to(socket.id).emit('upload status', UploadStatus.SUCCESS);
+      io.to(socket.id).emit('upload status', Symbol.keyFor(UploadStatus.SUCCESS));
     } else {
-      io.to(socket.id).emit('upload status', UploadStatus.ERROR);
+      io.to(socket.id).emit('upload status', Symbol.keyFor(UploadStatus.ERROR));
     }
 }
 
